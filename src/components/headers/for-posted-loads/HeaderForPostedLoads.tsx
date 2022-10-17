c1c51e6df9437027780dab8fe0a3ebb7be95d583
Yurii Komarnutskyi <kv281214@gmail.com>
2022-10-17 22:22:26 +0300

    Implemented scss in the component

--- src/components/headers/for-posted-loads/HeaderForPostedLoads.tsx
+++ src/components/headers/for-posted-loads/HeaderForPostedLoads.tsx
@@ -1,9 +1,9 @@
 import React from 'react';
-import styles from './HeaderForPostedLoads.module.css'
+import styles from './HeaderForPostedLoads.module.scss'
 const HeaderForPostedLoads = () => {
   return (
     <div className={styles.container}>
-      <div>Pick-up</div>
+      {/* <div>Pick-up</div>
       <div>Drop-off</div>
       <div className={styles.orig}>Origin</div>
       <div className={styles.dest}>Destination</div>
@@ -11,7 +11,17 @@ const HeaderForPostedLoads = () => {
       <div>F/P</div>
       <div>Length</div>
       <div>Weight</div>
-      <div>Offer Rate</div>
+      <div>Offer Rate</div> */}
+
+      <div className={styles.pu}>Pick-up</div>
+      <div className={styles.dropOff}>Drop-off</div>
+      <div className={styles.orig}>Origin</div>
+      <div className={styles.dest}>Destination</div>
+      <div className={styles.equipment}>Truck</div>
+      <div className={styles.fp}>F/P</div>
+      <div className={styles.len}>Length</div>
+      <div className={styles.weight}>Weight</div>
+      <div className={styles.price}>Offer Rate</div>
     </div>
   )
 }
